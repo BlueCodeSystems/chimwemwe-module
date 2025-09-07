cb75b4c6e306919d39de6855b98a72c8dfc200e8
Andre Lesa <alesa@bluecodeltd.com>
2025-09-07 11:26:01 +0200

    refactor(core): align FP profile contract with new FP library
    WHAT
    - Switch contract types from org.smartregister.chw.fp.* to org.smartregister.fp.* (ProfileContract, ClientDetail).
    - Update method signatures to use new FP domain model.
    
    WHY
    - Matches the migrated dependency coordinates and avoids type mismatches after dependency alignment.

--- opensrp-ecap-chw-core/src/main/java/org/smartregister/chw/core/contract/CoreFamilyPlanningMemberProfileContract.java
+++ opensrp-ecap-chw-core/src/main/java/org/smartregister/chw/core/contract/CoreFamilyPlanningMemberProfileContract.java
@@ -1,23 +1,23 @@
 package org.smartregister.chw.core.contract;
 
 import org.json.JSONObject;
-import org.smartregister.chw.fp.contract.BaseFpProfileContract;
-import org.smartregister.chw.fp.domain.FpMemberObject;
+import org.smartregister.fp.features.profile.contract.ProfileContract;
+import org.smartregister.fp.common.domain.ClientDetail;
 import org.smartregister.repository.AllSharedPreferences;
 
 public interface CoreFamilyPlanningMemberProfileContract {
 
-    interface View extends BaseFpProfileContract.View {
-        void startFormActivity(JSONObject formJson, FpMemberObject fpMemberObject);
+    interface View extends ProfileContract.View {
+        void startFormActivity(JSONObject formJson, ClientDetail clientDetail);
     }
 
-    interface Presenter extends BaseFpProfileContract.Presenter {
+    interface Presenter extends ProfileContract.Presenter {
         void createReferralEvent(AllSharedPreferences allSharedPreferences, String jsonString) throws Exception;
 
         void startFamilyPlanningReferral();
     }
 
-    interface Interactor extends BaseFpProfileContract.Interactor {
+    interface Interactor {
         void createReferralEvent(AllSharedPreferences allSharedPreferences, String jsonString, String entityID) throws Exception;
     }
 }
