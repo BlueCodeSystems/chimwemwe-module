f2c9290f7086ab860d0b5344707d681539085cd9
Andre Lesa <alesa@bluecodeltd.com>
2025-09-13 16:41:15 +0200

    feat(core:hia2): migrate HIA2 reports to ViewPager2 + TabLayoutMediator
    Replace legacy ViewPager with ViewPager2 in HIA2ReportsActivity and wire TabLayout via TabLayoutMediator. Add Hia2ViewPager2Adapter to host Daily, Draft Monthly, and Sent Monthly fragments. Update fragment retrieval and currentItem handling accordingly and adjust tab titles. Includes small touch-ups in DailyTalliesFragment, DraftMonthlyFragment, and SentMonthlyFragment to work smoothly with ViewPager2.

--- opensrp-ecap-chw-core/src/main/java/org/smartregister/chw/core/fragment/DailyTalliesFragment.java
+++ opensrp-ecap-chw-core/src/main/java/org/smartregister/chw/core/fragment/DailyTalliesFragment.java
@@ -69,7 +69,7 @@ public class DailyTalliesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.report_expandable_list_view, container, false);
         expandableListView = fragmentView.findViewById(R.id.expandable_list_view);
-        expandableListView.setBackgroundColor(getResources().getColor(org.smartregister.R.color.white));
+        expandableListView.setBackgroundColor(getResources().getColor(R.color.white));
 
         GetAllTalliesTask getAllTalliesTask = new GetAllTalliesTask();
         getAllTalliesTask.execute();
