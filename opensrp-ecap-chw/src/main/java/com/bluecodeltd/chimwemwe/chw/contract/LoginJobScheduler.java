31d4b8ef652489a9e6693dfbb5b497975c0aaaaa
joseph <josephphiri909@gmail.com>
2026-05-04 12:34:52 +0200

    Revert "Add Firebase RTDB facility Excel importer"
    This reverts commit e86ee69ba9813a8a4b6b7494a28c1636d6fb9835.

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/chimwemwe/chw/contract/LoginJobScheduler.java
+++
@@ -1,9 +0,0 @@
-package com.bluecodeltd.chimwemwe.chw.contract;
-
-public interface LoginJobScheduler {
-    void scheduleJobsPeriodically();
-
-    void scheduleJobsImmediately();
-
-    long getFlexValue(int value);
-}
